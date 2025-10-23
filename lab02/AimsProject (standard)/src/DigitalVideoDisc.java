343f8c17cabde812d9d82e9c5210f9490ae06b60
quangpt22 <pthanhqz2@gmail.com>
2025-10-23 13:56:00 +0700

    classifier/instance member

--- lab02/AimsProject (standard)/src/DigitalVideoDisc.java
+++ lab02/AimsProject (standard)/src/DigitalVideoDisc.java
@@ -5,6 +5,9 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+
 	public String getTitle() {
 		return title;
 	}
@@ -19,6 +22,9 @@ public class DigitalVideoDisc {
 	}
 	public float getCost() {
 		return cost;
+	}
+	public int getId() {
+		return id;
 	}
 	public void setTitle(String title) {
 		this.title = title;
@@ -38,12 +44,16 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -52,5 +62,7 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 }
